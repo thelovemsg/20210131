4ff1b0478a88c4fb4b3c79f0ed81b81bf8a6a282
Seonjun <thelovemsg@naver.com>
2021-01-31 21:01:56 +0900

    JavaScript : navbar

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+"use strict";
 
+//Make navbar
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  console.log(window.screenY);
+  console.log(`navbarHeight : ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
